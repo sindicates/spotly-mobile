b2df6ca65873f28f7f86566cc45b821b19fb732f
Nnadozi <chikaosro@gmail.com>
2026-08-12 00:24:53 -0400

    initial commit

--- nativewind-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="nativewind/types" />
-
-// Metro + NativeWind resolve the stylesheet at build time; TypeScript only
-// needs to know the side-effect import is legal.
-declare module '*.css';
